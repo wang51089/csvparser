6675c356f722f0ebd7014a4bd4e1038385978f8a
wjh <454916242@qq.com>
2016-01-16 23:52:55 +0800

    the initial commitment

---
+++ src/main/java/com/hohai/jx/wjh/main/Main.java
@@ -0,0 +1,12 @@
+package com.hohai.jx.wjh.main;
+
+/**
+ * Created by wjh on 2016/1/16.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("=============");
+        System.out.println(args.toString());
+        System.out.println("=============");
+    }
+}
